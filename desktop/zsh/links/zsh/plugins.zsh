e5f815119193062d994687c97ef323365deacf6a
Fabio Forni <development@redaril.me>
2023-02-16 21:02:15 +0100

    zsh: Do not set XDG directories. Base does this already

---
+++ desktop/zsh/links/zsh/plugins.zsh
@@ -0,0 +1,10 @@
+fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
+source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-gist.githubusercontent.com-SLASH-jpouellet-SLASH-5278239 )
+source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-gist.githubusercontent.com-SLASH-jpouellet-SLASH-5278239/zbell.sh
