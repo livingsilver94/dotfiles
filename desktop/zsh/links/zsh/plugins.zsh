94d51ac27e7e142b99ffe1adb8b2316090661955
Fabio Forni <development@redaril.me>
2025-02-16 15:06:48 +0100

    desktop/zsh: Update plugins

--- desktop/zsh/links/zsh/plugins.zsh
+++ desktop/zsh/links/zsh/plugins.zsh
@@ -1,10 +1,10 @@
-fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
-source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
-fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
-source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
-fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
-source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-gist.githubusercontent.com-SLASH-jpouellet-SLASH-5278239 )
-source /home/fabio/.cache/antidote/https-COLON--SLASH--SLASH-gist.githubusercontent.com-SLASH-jpouellet-SLASH-5278239/zbell.sh
+fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/F-Sy-H.plugin.zsh
+fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-gist.githubusercontent.com-SLASH-jpouellet-SLASH-5278239 )
+source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-gist.githubusercontent.com-SLASH-jpouellet-SLASH-5278239/zbell.sh
